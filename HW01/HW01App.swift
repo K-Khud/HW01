f2aceae45a62425551e589f529d9a0380871e35e
Ekaterina Khudzhamkulova <katerina.koreneva@gmail.com>
2021-12-04 22:05:10 +0200

    Initial Commit

---
+++ HW01/HW01App.swift
@@ -0,0 +1,17 @@
+//
+//  HW01App.swift
+//  HW01
+//
+//  Created by Ekaterina Khudzhamkulova on 4.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct HW01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
